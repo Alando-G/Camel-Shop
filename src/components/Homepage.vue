dc1305b1b97ad82a0f8a0ce21d2c8b38ba440382
Alando-G <ivanalando101@gmail.com>
2024-06-20 22:28:17 +0300

    20/6/2024

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -39,28 +39,89 @@ export default {
 </script>
 
 <template>
-<h1>1vxn  Shop</h1>
-<div class ="Centre">
-  <img v-bind:src="image" width="500" height="300">
-</div>
-<div class="centre">
-  <a :href="url">Vue JS Practice</a>
-  <h3>Milk, White, dark</h3>
-  <h5>A wide variety of fine snacks</h5>
-</div>
-<div class="centre">
-  <u1>
-    <li :key="item" v-for="item in stockDetails">
-      <div>
-        <img v-bind:src="item.itemImage" width="200" height="150">
-      </div>
-      <div>
-      <span>{{ item.itemName }}</span> &nbsp; &nbsp;
-      <span><button style="background-color:blueviolet; color:white; border: blueviolet;">+</button></span>
-    </div>
-    <br/>
-    </li>
-  </u1>
-</div>
+<v-card
+    class="mx-auto"
+    width="1100"
+  >
+    
+    <v-carousel>
+  <v-carousel-item
+    src="./../images/vinicius-amnx-amano-dbOV1qSiL-c-unsplash.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="./../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg"
+    cover
+  ></v-carousel-item>
+
+  <v-carousel-item
+    src="./../images/customerbox-aXq1oCCjlVM-unsplash.jpg"
+    cover
+  ></v-carousel-item>
+</v-carousel>
+    
+  </v-card>
+  <v-card align="center">
+        <v-card-item>
+          <v-card-title>1vxn Candy Shop</v-card-title>
+
+          <v-card-subtitle>The no.1 Candy Shop</v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          Welcome to 1vxn Candy Shop, your ultimate destination for all things sweet and delightful! At Sweet Delights, we believe in the power of candy to brighten your day, evoke fond memories, and create moments of pure joy.
+
+Our shop features an extensive selection of candies from around the world, carefully curated to satisfy every taste bud. From traditional favorites like gummy bears and licorice twists to artisanal chocolates and nostalgic treats, we have something for everyone. Whether you're planning a party, looking for a unique gift, or simply treating yourself, Sweet Delights is your go-to candy paradise.
+
+We pride ourselves on offering top-quality products and exceptional customer service. Our knowledgeable staff is passionate about candy and eager to help you find the perfect sweet treat. We also prioritize freshness and variety, ensuring that our shelves are always stocked with the latest trends and timeless classics.
+        </v-card-text>
+      </v-card>
+
+      <v-row>
+    <v-col cols="12" md="4">
+      <v-card
+        subtitle="This is a card subtitle"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+        title="This is a title"
+      ></v-card>
+
+      <div class="text-center text-caption">Using Props Only</div>
+    </v-col>
+
+    <v-col cols="12" md="4">
+      <v-card>
+        <template v-slot:title>
+          James Chapo
+        </template>
+
+        <template v-slot:subtitle>
+          Fred mutura
+        </template>
+
+        <template v-slot:text>
+          Leonel Mtungi
+        </template>
+      </v-card>
+
+      <div class="text-center text-caption">Using Slots Only</div>
+    </v-col>
+
+    <v-col cols="12" md="4">
+      <v-card>
+        <v-card-item>
+          <v-card-title>This is a title</v-card-title>
+
+          <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+        </v-card-text>
+      </v-card>
+
+      <div class="text-center text-caption">Using Markup Only</div>
+    </v-col>
+  </v-row>
 
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,11 +7,11 @@
   <v-app-bar-title>Camel shop</v-app-bar-title>
   <v-btn to="/">Home</v-btn>
   <v-btn to="/Products">Products</v-btn> 
-  <v-btn to="/About us">About us</v-btn> 
   <v-btn to="/Cart">Cart</v-btn> 
   <v-btn to="/Contact us">Contact us</v-btn> 
+  <v-btn to="/About us">About us</v-btn> 
   <v-btn to="/Servicesofferd">Services offerd</v-btn> 
-  <v-btn to="/Navbar">Navbar</v-btn> 
+  <v-btn to="/Navbar"></v-btn> 
   
 </v-app-bar>
 
