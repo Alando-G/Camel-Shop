762652fa45e0c9ab286ea9d3e1ba8a90639a263f
Alando-G <ivanalando101@gmail.com>
2024-06-24 09:11:16 +0300

    Images

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -113,7 +113,7 @@ export default {
                 itemName : "Chocolates",
                 itemPrice : 10,
                 itemQuantity : 0,
-                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                itemImage: '../images/side-view-decorated-chocolate-candy-black-gold-box.jpg',
                 description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
 
             }
@@ -123,7 +123,7 @@ export default {
                 itemName : "Crisps",
                 itemPrice : 10,
                 itemQuantity : 4,
-                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                itemImage: '../images/27373.jpg',
                 description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
 
             }
@@ -133,7 +133,7 @@ export default {
                 itemName : "Popcorn",
                 itemPrice : 10,
                 itemQuantity : 4,
-                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                itemImage: '../images/2207.i305.026.S.m005.c13.realistic popcorn poster.jpg',
                 description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
 
             }
@@ -143,7 +143,7 @@ export default {
                 itemName : "Bites",
                 itemPrice : 10,
                 itemQuantity : 4,
-                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                itemImage: '../images/images.jpg',
                 description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
 
             }
@@ -153,7 +153,7 @@ export default {
                 itemName : "Dried Fruit",
                 itemPrice : 10,
                 itemQuantity : 4,
-                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                itemImage: '../images/various-dried-fruits-dates-plums-raisins-figs.jpg',
                 description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
 
             },
@@ -162,27 +162,27 @@ export default {
                 itemName : "Macaroons",
                 itemPrice : 10,
                 itemQuantity : 4,
-                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                itemImage: '../images/delicious-macarons-table.jpg',
                 description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
 
             }
             ,
             {
                 itemNumber: 27466,
-                itemName : "Lollipop",
+                itemName : "Chewing gum",
                 itemPrice : 10,
                 itemQuantity : 4,
-                itemImage: '../images/customerbox-aXq1oCCjlVM-unsplash.jpg',
+                itemImage: '../images/Screenshot 2024-06-24 091001.jpg',
                 description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
 
             }
             ,
             {
                 itemNumber: 27466,
-                itemName : "Lollipop",
+                itemName : "Fruit Juice",
                 itemPrice : 10,
                 itemQuantity : 4,
-                itemImage: '../images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg',
+                itemImage: '../images/orange-juice-with-mint-orange-lime-lemon-strawberry-cherry-coconut-grapefruit-goblet-brick-stone-wooden-surface-side-view.jpg',
                 description: "Our sweets are wonderfully crafted by the best ingrdients to bring deliciousness to your parties"
 
             }
